c20234c32a1ab5cfa6397c95ab6c02fddc05d7f0
Jucelio coelho <juceliocoelho2022@gmail.com>
2024-02-26 08:57:29 -0300

    validation incluido

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,12 @@
 INSERT INTO tb_user (name, password) VALUES ('Maria', '12763721371263821');
-INSERT INTO tb_user (name, password) VALUES ('Joao', '12763721371263821');
+INSERT INTO tb_user (name, password) VALUES ('Joao', '127637271371263821');
+INSERT INTO tb_user (name, password) VALUES ('pedro', '127637271371263821');
+INSERT INTO tb_user (name, password) VALUES ('marcio', '127637213771263821');
+INSERT INTO tb_user (name, password) VALUES ('Mauro', '127637213777263821');
+INSERT INTO tb_user (name, password) VALUES ('Lilian', '12763721371263821');
+INSERT INTO tb_user (name, password) VALUES ('Elaine', '127637213767263821');
+INSERT INTO tb_user (name, password) VALUES ('Ana', '127637216371263821');
+INSERT INTO tb_user (name, password) VALUES ('Jose', '12763721371263821');
+INSERT INTO tb_user (name, password) VALUES ('Antonio', '127637217371263821');
+INSERT INTO tb_user (name, password) VALUES ('Robison', '127637217371263821');
+
